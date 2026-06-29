4b6300fb68cd54085170faeff4156a5e96f6eb82
Tony Salinas <kroosismo0202@gmail.com>
2026-06-29 13:38:46 -0400

    Añadido microservicio de registro y de configuraciones

--- employee-service/src/main/java/com/tony/employee_service/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/tony/employee_service/EmployeeServiceApplication.java
@@ -2,9 +2,11 @@ package com.tony.employee_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
